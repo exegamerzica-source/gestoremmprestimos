ab3c57c99c54120102cc8c25e0f521d4fd61a457
exegamerzica-source <exegamerzica@gmail.com>
2026-06-07 05:05:30 -0300

    committ

--- supabase-config.js
+++ supabase-config.js
@@ -1,6 +1,6 @@
 window.SISTEMA_EMPRESTIMOS_CONFIG = {
-  supabaseUrl: "sb_publishable_OtNd45-eYIYStux5oZo2EA_iQsGggFY",
-  supabaseAnonKey: "sb_secret_QmS0GeU7oR2OBFPiT_x4Ew_pjZM9ltD",
+  supabaseUrl: "https://ficstjzakarzyamrhsah.supabase.co",
+  supabaseAnonKey: "sb_publishable_OtNd45-eYIYStux5oZo2EA_iQsGggFY",
   stateTable: "dashboard_state",
   storageBucket: "client-files"
 };
